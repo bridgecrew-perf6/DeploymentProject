24f56d76ca159fb6fd525e60abea47fdc331f08e
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-03-22 22:33:27 +0200

    updating the env variables

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,3 +1,3 @@
-eb setenv POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgres POSTGRES_DB=postgres PORT=8426 DB_PORT=5432 POSTGRES_HOST=david.cz7nju4k84bw.us-east-1.rds.amazonaws.com RDS_DIALECT=postgres AWS_REGION=us-east-1 URL=http://udagram-api-env-david.eba-smsy9rs9.us-east-1.elasticbeanstalk.com/ JWT_SECRET=hello-world-8426 AWS_BUCKET=dazy-bucket
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB PORT=$PORT DB_PORT=$DB_PORT POSTGRES_HOST=$POSTGRES_HOST RDS_DIALECT=$RDS_DIALECT AWS_REGION=$AWS_REGION URL=$URL JWT_SECRET=$JWT_SECRET AWS_BUCKET=$AWS_BUCKET
 eb deploy udagram-api-env-david
 eb health && eb status
